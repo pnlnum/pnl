e43ee3ef21c8f58c5093df5086797c695e292e05
Jerome Lelong <jerome.lelong@gmail.com>
2010-01-27 13:41:26 +0000

    adding libcephes
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pnl/trunk@164 94367915-3dfb-0310-9a54-a1fefb3e5df9

---
+++ Src/libpnl/src/libcephes/pnl_names.h
@@ -0,0 +1,12 @@
+#ifndef _PNL_NAMES_
+#define _PNL_NAMES_
+
+#define Gamma pnl_sf_gamma
+#define lgam pnl_sf_log_gamma
+#define erf pnl_sf_erf
+#define erfc pnl_sf_erfc
+#define igam pnl_sf_gamma_inc_Q
+#define igamc pnl_sf_gamma_inc_P
+#define expn pnl_sf_expint_En
+
+#endif /* _PNL_NAMES_ */
