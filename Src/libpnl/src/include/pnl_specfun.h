e718de106c09ceb08cae4249ff521245fa115f5c
Jerome Lelong <jerome.lelong@gmail.com>
2010-01-28 14:41:25 +0000

    hypergeometric functions
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pnl/trunk@181 94367915-3dfb-0310-9a54-a1fefb3e5df9

--- Src/libpnl/src/include/pnl_specfun.h
+++ Src/libpnl/src/include/pnl_specfun.h
@@ -10,13 +10,26 @@ extern "C" {
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "pnl_bessel.h"
+
 extern double pnl_sf_gamma_inc(double a,double x);
 extern double pnl_sf_gamma_inc_P(double a,double x);
 extern double pnl_sf_gamma_inc_Q(double a,double x);
 extern double pnl_sf_expint_En(int n,double x);
+/* extern double pnl_sf_expint_Ei(double x); */
 
 extern double pnl_sf_gamma (double);
 extern double pnl_sf_log_gamma (double);
+extern double pnl_sf_log_erf (double x);
+extern double pnl_sf_log_erfc (double x);
+extern double pnl_sf_erf (double x);
+extern double pnl_sf_erfc (double x);
+
+extern double pnl_sf_hyperg_2F1 (double a, double b, double c, double x);
+extern double pnl_sf_hyperg_1F1 (double a, double b, double x);
+extern double pnl_sf_hyperg_2F0 (double a, double b, double x);
+extern double pnl_sf_hyperg_0F1 (double c, double x);
+
 
 #ifdef __cplusplus
 }
--- Src/libpnl/src/libcephes/cephes_wrappers.c
+++ Src/libpnl/src/libcephes/cephes_wrappers.c
@@ -1,6 +1,6 @@
 
 /************************************************************************/
-/* Copyright JÃ©rÃ´me Lelong <jerome.lelong@gmail.com>                    */
+/* Copyright Jérôme Lelong <jerome.lelong@gmail.com>                    */
 /*                                                                      */
 /* This program is free software: you can redistribute it and/or modify */
 /* it under the terms of the GNU Lesser General Public License as       */
@@ -20,8 +20,62 @@
 #include "pnl_mathtools.h"
 #include "pnl_specfun.h"
 
+
+extern double hyp2f0 (double, double, double, int, double*);
+
 double pnl_sf_gamma_inc (double a, double x)
 {
-  return  pnl_sf_gamma_inc_Q (a, x) * pnl_sf_gamma (a);
+  if (x == 0.)
+    {
+      return pnl_sf_gamma (a);
+    }
+  else if (a < 0)
+    {
+      double shift = -pow (x, a) * exp (-x);
+      return (shift +  pnl_sf_gamma_inc (a + 1., x)) / a;
+    }
+  else if (a == 0.)
+    {
+      return pnl_sf_expint_En (1, x);
+    }
+  else
+    {
+    return  pnl_sf_gamma_inc_Q (a, x) * pnl_sf_gamma (a);      
+    }
+}
+
+/* double pnl_sf_expint_Ei (double x)
+ * {
+ *   return - pnl_sf_expint_En (1, -x);
+ * } */
+
+double pnl_sf_log_erf (double x)
+{
+  return log (pnl_sf_erf (x));
+}
+
+double pnl_sf_log_erfc (double x)
+{
+  return log (pnl_sf_erfc (x));
+}
+
+double pnl_sf_hyperg_2F0 (double a, double b, double x)
+{
+  int type;
+  double err;
+  type = 2;
+  return hyp2f0 (a, b, x, type, &err);
+}
+
+double pnl_sf_hyperg_0F1 (double v, double x)
+{
+  double num, den;
+
+  if (x==0) return 1.0;
+  
+  num = pnl_bessel_i (v-1,2*sqrt(x));
+  den = pow (x, ((v-1.0)/2.0));
+  num *= pnl_sf_gamma(v);
+  return num / (den);
 }
 
