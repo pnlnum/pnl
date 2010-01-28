990db1ac1dac417466173e99023ed697daa3642a
Jerome Lelong <jerome.lelong@gmail.com>
2010-01-28 14:17:09 +0000

    cephes headers
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pnl/trunk@180 94367915-3dfb-0310-9a54-a1fefb3e5df9

--- Src/libpnl/src/libcephes/pnl_names.h
+++ Src/libpnl/src/libcephes/pnl_names.h
@@ -5,8 +5,11 @@
 #define lgam pnl_sf_log_gamma
 #define erf pnl_sf_erf
 #define erfc pnl_sf_erfc
-#define igam pnl_sf_gamma_inc_Q
-#define igamc pnl_sf_gamma_inc_P
+#define igam pnl_sf_gamma_inc_P
+#define igamc pnl_sf_gamma_inc_Q
 #define expn pnl_sf_expint_En
+#define hyp2f1 pnl_sf_hyperg_2F1
+#define hyperg pnl_sf_hyperg_1F1
+
 
 #endif /* _PNL_NAMES_ */
