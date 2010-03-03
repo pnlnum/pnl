d469c36e356ff5b533687b835d8a07f37a600adc
Jerome Lelong <jerome.lelong@gmail.com>
2010-03-03 13:21:03 +0000

    duplicate
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pnl/trunk@272 94367915-3dfb-0310-9a54-a1fefb3e5df9

--- Src/libpnl/src/include/pnl_internals.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PNL_INTERNALS_H
-#define _PNL_INTERNALS_H
-
-#define FREE(x) if (x != NULL) { free (x); x = NULL; }
-#define MALLOC_DOUBLE(n) malloc ((n) * sizeof (double))
-#define MALLOC_INT(n) malloc ((n) * sizeof (int))
-
-#endif /* _PNL_INTERNALS_H */
