c087127bf3f480a152fd901eaf42b1e101ae2189
Jerome Lelong <jerome.lelong@gmail.com>
2010-03-04 14:38:52 +0000

    test of the differentiation of a regression fitting
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pnl/trunk@284 94367915-3dfb-0310-9a54-a1fefb3e5df9

--- examples/basis_test.c
+++ examples/basis_test.c
@@ -24,6 +24,7 @@
 #include "pnl_vector.h"
 #include "pnl_basis.h"
 #include "pnl_mathtools.h"
+#include "pnl_random.h"
 
 #define function exp
 
@@ -43,7 +44,7 @@ static void exp_regression2()
 
   PnlBasis *basis;
 
-  printf("\nRegression of the exponential function on [0:0.05:5]\n");
+  printf("\n\n** Regression of the exponential function on [0:0.05:5] ** \n\n");
 
   alpha = pnl_vect_create (0);
 
@@ -104,7 +105,7 @@ static void regression_multid()
   PnlVect *alpha;
 
   PnlBasis *basis;
-  printf("\nMulti dimensional regression on log (1+x[0]*x[0] + x[1]*x[1])\n");
+  printf("\n\n** Multi dimensional regression on log (1+x[0]*x[0] + x[1]*x[1]) **\n\n");
   alpha = pnl_vect_create (0);
 
   /* creating the grid */
@@ -153,9 +154,123 @@ static void regression_multid()
 }
 
 
+static double fonction_a_retrouver(double t, double x)
+{
+  return (1-t)*x*x/5;
+}
+
+static double derive_x_fonction_a_retrouver(double t, double x)
+{
+  return (1-t)*2*x/5;
+}
+
+static double derive_xx_fonction_a_retrouver(double t, double x)
+{
+  return (1-t)*2/5;
+}
+
+static double derive_t_fonction_a_retrouver(double t, double x)
+{
+  return -x*x/5;
+}
+
+static double derive_x_approx_fonction(PnlBasis *B, PnlVect *alpha, double t, double x)
+{
+  double arg[2];
+  arg[0] = t; arg[1] = x;
+  return pnl_basis_eval_D (B, alpha, arg, 2); 
+}
+
+static void derive_approx_fonction(PnlBasis *B, PnlVect *D, PnlVect *alpha, double t, double x)
+{
+  double sum0, sum1, sum2, sum3;
+  double arg[2];
+  arg[0] = t; arg[1] = x;
+  sum0=0.0;//calcule la valeur de la fonction
+  sum1=0.0;//calcule la valeur de sa derivee en x
+  sum2=0.0;//calcule la valeur de sa derivee seconde en x
+  sum3=0.0;//calcule la valeur de sa derivee en t
+
+  sum0 = pnl_basis_eval (B, alpha, arg);
+  sum1 = pnl_basis_eval_D (B, alpha, arg, 2);
+  sum2 = pnl_basis_eval_D2 (B, alpha, arg, 2, 2);
+  sum3 = pnl_basis_eval_D (B, alpha, arg, 1);
+
+  LET(D,0)=sum0;
+  LET(D,1)=sum1;
+  LET(D,2)=sum2;
+  LET(D,3)=sum3;
+}
+
+static void pnl_basis_eval_test ()
+{
+  PnlMat *X;
+  PnlVect *V,*x,*t,*D, *alpha;
+  PnlBasis *basis;
+  int j,m,n;
+  double t0,x0;
+
+  printf ("\n\n** Differentation of the regression **\n\n");
+  m=19;//nombre de polynomes
+  n=50;
+  D=pnl_vect_create(4);
+  x=pnl_vect_create(n);
+  t=pnl_vect_create(n);
+  t0=0.5;
+  x0=2.5;
+  //on tire aléatoirement les points auxquels on va évaluer la fonction à
+  //retrouver
+  pnl_vect_rand_uni(x,n,-5,4,7);
+  pnl_vect_rand_uni(t,n,0,1,7);
+  basis = pnl_basis_init (HERMITIAN, m, 2);
+  alpha = pnl_vect_create (m);
+  X = pnl_mat_create (n, 2);
+  for(j=0;j<n;j++)
+    {
+      MLET (X, j, 0) = GET(t,j);
+      MLET (X, j, 1) = GET(x,j);
+    }
+  V=pnl_vect_create(n);
+  //création du vecteur contenant les valeurs de la fonction à retrouver aux
+  //points t(j),x(j)
+  for(j=0;j<n;j++)
+    {
+      LET(V,j)=fonction_a_retrouver(GET(t,j),GET(x,j));
+    }
+  pnl_basis_fit_ls (basis, alpha, X, V);
+  /* calcul des valeurs approchées des dérivées de la fonction à retrouver (1
+     en temps et 2 en espace) */
+  derive_approx_fonction(basis, D, alpha,t0,x0);
+  printf("valeur exacte de la fonction : %f\n",
+         fonction_a_retrouver(t0,x0));
+  printf("valeur approchee de la fonction :%f \n\n",
+         pnl_vect_get(D,0));
+
+  printf("valeur exacte de la derivee de la fonction : %f\n",
+         derive_x_fonction_a_retrouver(t0,x0));
+  printf("valeur approchee de la derivee de la fonction :%f \n\n",
+         derive_x_approx_fonction(basis, alpha, t0, x0));
+
+  printf("valeur exacte de la derivee seconde en espace fonction : %f\n",
+         derive_xx_fonction_a_retrouver(t0,x0));
+  printf("valeur approchee de la derivee seconde en espace de la fonction  :%f \n\n",
+         pnl_vect_get(D,2));
+
+  printf("valeur exacte de la derivee en temps fonction :%f\n",
+         derive_t_fonction_a_retrouver(t0,x0));
+  printf("valeur approchee de la derivee en temps de la fonction  :%f \n",
+         pnl_vect_get(D,3));
+  pnl_vect_free(&x);
+  pnl_vect_free(&t);
+  pnl_vect_free(&V);
+  pnl_vect_free(&D);
+  pnl_mat_free(&X);
+}
+
+
 void basis_test ()
 {
-  /* exp_regression(); */
-  exp_regression2();
-  regression_multid();
+  exp_regression2 ();
+  regression_multid ();
+  pnl_basis_eval_test ();
 }
