9e42bd9f5f126b886d3d1f5648cc81aafad941cb
Jerome Lelong <jerome.lelong@gmail.com>
2010-05-03 16:13:15 +0000

    QuadPack
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pnl/trunk@360 94367915-3dfb-0310-9a54-a1fefb3e5df9

--- src/include/pnl_integration.h
+++ src/include/pnl_integration.h
@@ -8,32 +8,39 @@ extern "C" {
 
 #include "pnl_mathtools.h"
 
-/* Gauss-Kronrod-Patterson quadrature coefficients for use in
-   quadpack routine qng. These coefficients were calculated with
-   101 decimal digit arithmetic by L. W. Fullerton, Bell Labs, Nov
-   1981. */
+extern double pnl_integration (PnlFunc *F, double x0, double x1, 
+                  int n, char *meth);
 
-extern int pnl_integration_GK (const PnlFunc *F,
-                               double x0, double x1,
-                               double epsabs, double epsrel,
-                               double * result, double * abserr,
-                               int * neval);
+extern double pnl_integration_2d (PnlFunc2D *F, double x0, double x1,
+                  double y0,double y1, int nx, int ny, char *meth);
 
-extern int pnl_integration_GK2D (const PnlFunc2D *F,
-                                 double x0, double x1,
-                                 double y0,double y1,
-                                 double epsabs, double epsrel,
-                                 double * result, double * abserr,
-                                 int * neval);
+extern int pnl_integration_qng (PnlFunc *F, double x0, double x1,
+                  double epsabs, double epsrel, double * result,
+                  double * abserr, int * neval);
 
+extern int pnl_integration_GK (PnlFunc *F, double x0, double x1,
+                  double epsabs, double epsrel, double * result,
+                  double * abserr, int * neval);
 
-extern double pnl_integration (const PnlFunc *F, double x0, double x1, int n, char *meth);
-extern double pnl_integration_2D (const PnlFunc2D *F, double x0, double x1,
-                                  double y0,double y1, int nx, int ny, char *meth);
+extern int pnl_integration_qng_2d (PnlFunc2D *F, double x0, double x1,
+                  double y0,double y1, double epsabs, double epsrel,
+                  double * result, double * abserr, int * neval);
+
+extern int pnl_integration_GK2D (PnlFunc2D *F, double x0, double x1,
+                  double y0,double y1, double epsabs, double epsrel,
+                  double * result, double * abserr, int * neval);
+
+extern int pnl_integration_qag (PnlFunc *f, double a, double b,
+                   double epsabs, double epsrel, int limit, double *result, 
+                   double *abserr, int *neval);
+
+extern int pnl_integration_qagp (PnlFunc *f, double a, double b, const PnlVect *singularities,
+                   double epsabs, double epsrel, int limit, double *result, 
+                   double *abserr, int *neval);
+
 
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
-
 
 #endif /* _PNL_INTEGRATION_H */
