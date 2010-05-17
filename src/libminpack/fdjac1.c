c8679f3b438ae877ac95111bc19623d601d7358d
Jerome Lelong <jerome.lelong@gmail.com>
2010-05-17 08:19:15 +0000

    update doc pnl_root
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pnl/trunk@391 94367915-3dfb-0310-9a54-a1fefb3e5df9

--- src/libminpack/fdjac1.c
+++ src/libminpack/fdjac1.c
@@ -54,6 +54,89 @@
 #include <math.h>
 #include "cminpack.h"
 
+/* subroutine fdjac1
+ * 
+ * this subroutine computes a forward-difference approximation
+ * to the n by n jacobian matrix associated with a specified
+ * problem of n functions in n variables. if the jacobian has
+ * a banded form, then function evaluations are saved by only
+ * approximating the nonzero terms.
+ * 
+ * the subroutine statement is
+ * 
+ *   subroutine pnl_minpack_fdjac1(fcn,n,x,fvec,fjac,ldfjac,iflag,ml,mu,epsfcn,
+ *                     wa1,wa2)
+ * 
+ * where
+ * 
+ *   fcn is the name of the user-supplied subroutine which
+ *     calculates the functions. fcn must be declared
+ *     in an external statement in the user calling
+ *     program, and should be written as follows.
+ * 
+ *     subroutine fcn(n,x,fvec,iflag)
+ *     integer n,iflag
+ *     double precision x(n),fvec(n)
+ *     ----------
+ *     calculate the functions at x and
+ *     return this vector in fvec.
+ *     ----------
+ *     return
+ *     end
+ * 
+ *     the value of iflag should not be changed by fcn unless
+ *     the user wants to terminate execution of fdjac1.
+ *     in this case set iflag to a negative integer.
+ * 
+ *   n is a positive integer input variable set to the number
+ *     of functions and variables.
+ * 
+ *   x is an input array of length n.
+ * 
+ *   fvec is an input array of length n which must contain the
+ *     functions evaluated at x.
+ * 
+ *   fjac is an output n by n array which contains the
+ *     approximation to the jacobian matrix evaluated at x.
+ * 
+ *   ldfjac is a positive integer input variable not less than n
+ *     which specifies the leading dimension of the array fjac.
+ * 
+ *   iflag is an integer variable which can be used to terminate
+ *     the execution of fdjac1. see description of fcn.
+ * 
+ *   ml is a nonnegative integer input variable which specifies
+ *     the number of subdiagonals within the band of the
+ *     jacobian matrix. if the jacobian is not banded, set
+ *     ml to at least n - 1.
+ * 
+ *   epsfcn is an input variable used in determining a suitable
+ *     step length for the forward-difference approximation. this
+ *     approximation assumes that the relative errors in the
+ *     functions are of the order of epsfcn. if epsfcn is less
+ *     than the machine precision, it is assumed that the relative
+ *     errors in the functions are of the order of the machine
+ *     precision.
+ * 
+ *   mu is a nonnegative integer input variable which specifies
+ *     the number of superdiagonals within the band of the
+ *     jacobian matrix. if the jacobian is not banded, set
+ *     mu to at least n - 1.
+ * 
+ *   wa1 and wa2 are work arrays of length n. if ml + mu + 1 is at
+ *     least n, then the jacobian is considered dense, and wa2 is
+ *     not referenced.
+ * 
+ * subprograms called
+ * 
+ *   minpack-supplied ... dpmpar
+ * 
+ *   fortran-supplied ... dabs,dmax1,dsqrt
+ * 
+ * argonne national laboratory. minpack project. march 1980.
+ * burton s. garbow, kenneth e. hillstrom, jorge j. more
+ */
+
 
  int pnl_minpack_fdjac1(minpack_func_nn fcn, void *p, int n, double *x, const double *
 	fvec, double *fjac, int ldfjac, int ml, 
@@ -70,91 +153,6 @@
     double epsmch;
     int iflag = 0;
 
-/*     ********** */
-
-/*     subroutine fdjac1 */
-
-/*     this subroutine computes a forward-difference approximation */
-/*     to the n by n jacobian matrix associated with a specified */
-/*     problem of n functions in n variables. if the jacobian has */
-/*     a banded form, then function evaluations are saved by only */
-/*     approximating the nonzero terms. */
-
-/*     the subroutine statement is */
-
-/*       subroutine pnl_minpack_fdjac1(fcn,n,x,fvec,fjac,ldfjac,iflag,ml,mu,epsfcn, */
-/*                         wa1,wa2) */
-
-/*     where */
-
-/*       fcn is the name of the user-supplied subroutine which */
-/*         calculates the functions. fcn must be declared */
-/*         in an external statement in the user calling */
-/*         program, and should be written as follows. */
-
-/*         subroutine fcn(n,x,fvec,iflag) */
-/*         integer n,iflag */
-/*         double precision x(n),fvec(n) */
-/*         ---------- */
-/*         calculate the functions at x and */
-/*         return this vector in fvec. */
-/*         ---------- */
-/*         return */
-/*         end */
-
-/*         the value of iflag should not be changed by fcn unless */
-/*         the user wants to terminate execution of fdjac1. */
-/*         in this case set iflag to a negative integer. */
-
-/*       n is a positive integer input variable set to the number */
-/*         of functions and variables. */
-
-/*       x is an input array of length n. */
-
-/*       fvec is an input array of length n which must contain the */
-/*         functions evaluated at x. */
-
-/*       fjac is an output n by n array which contains the */
-/*         approximation to the jacobian matrix evaluated at x. */
-
-/*       ldfjac is a positive integer input variable not less than n */
-/*         which specifies the leading dimension of the array fjac. */
-
-/*       iflag is an integer variable which can be used to terminate */
-/*         the execution of fdjac1. see description of fcn. */
-
-/*       ml is a nonnegative integer input variable which specifies */
-/*         the number of subdiagonals within the band of the */
-/*         jacobian matrix. if the jacobian is not banded, set */
-/*         ml to at least n - 1. */
-
-/*       epsfcn is an input variable used in determining a suitable */
-/*         step length for the forward-difference approximation. this */
-/*         approximation assumes that the relative errors in the */
-/*         functions are of the order of epsfcn. if epsfcn is less */
-/*         than the machine precision, it is assumed that the relative */
-/*         errors in the functions are of the order of the machine */
-/*         precision. */
-
-/*       mu is a nonnegative integer input variable which specifies */
-/*         the number of superdiagonals within the band of the */
-/*         jacobian matrix. if the jacobian is not banded, set */
-/*         mu to at least n - 1. */
-
-/*       wa1 and wa2 are work arrays of length n. if ml + mu + 1 is at */
-/*         least n, then the jacobian is considered dense, and wa2 is */
-/*         not referenced. */
-
-/*     subprograms called */
-
-/*       minpack-supplied ... dpmpar */
-
-/*       fortran-supplied ... dabs,dmax1,dsqrt */
-
-/*     argonne national laboratory. minpack project. march 1980. */
-/*     burton s. garbow, kenneth e. hillstrom, jorge j. more */
-
-/*     ********** */
     /* Parameter adjustments */
     --wa2;
     --wa1;
