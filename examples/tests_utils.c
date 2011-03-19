bd1279a469f6c7632210ff8935d8f19cecc34dd6
Jerome Lelong <jerome.lelong@gmail.com>
2011-03-19 07:31:26 +0000

    new unit test framework
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pnl/trunk@657 94367915-3dfb-0310-9a54-a1fefb3e5df9

--- examples/tests_utils.c
+++ examples/tests_utils.c
@@ -20,7 +20,141 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
+#include "pnl/pnl_mathtools.h"
 #include "tests_utils.h"
+
+int verbose = 0;
+int count_tests;
+int count_ok;
+int count_fail;
+
+/** 
+ * Initializes test counting system
+ */
+void pnl_init_tests ()
+{
+  count_tests = 0;
+  count_ok = 0;
+  count_fail = 0;
+}
+
+
+/** 
+ * Update test counter according to status
+ * 
+ * @param status an integer. If 0 last test is considered as past and
+ * FAILEd otherwise
+ */
+static void update_count_tests (int status)
+{
+  count_tests ++;
+  if ( status )
+    {
+      count_fail ++;
+    }
+  else
+    {
+      count_ok ++;
+    }
+}
+
+/** 
+ * Prints a summary of the tests
+ * 
+ * @return 0 if all tests were succeddful and 1 otherwise
+ */
+int pnl_finalize_tests (const char *str)
+{
+  printf ("%s : %s (TOTAL : %d, PASSED : %d, FAILED %d)\n", str, (count_fail == 0) ? "OK" : "FAIL", count_tests, count_ok, count_fail);
+  return ( count_fail >0 );
+}
+
+/** 
+ * Checks if |x -y| / |y| < relerr
+ * 
+ * @param x computed result
+ * @param y exepcted value
+ * @param relerr maximum relative error
+ * @param str the fonctionnality tested
+ * 
+ * @return FALSE or TRUE
+ */
+int pnl_eq_rel (double x, double y, double relerr, const char *str, const char *fmt, ...)
+{
+  int status = 0;
+  if ( (pnl_isnan (x) && !pnl_isnan(y)) ||
+       (pnl_isinf (x) && !pnl_isinf(y)) )
+    {
+      status = 1;
+    }
+  else if ( y != 0 )
+    {
+      status = (fabs (x -y) / fabs(y) > relerr);
+    }
+  else
+    {
+      status = (fabs(y) > relerr);
+    }
+  if ( status || verbose )
+    {
+      printf ("%s : ", str);
+      printf ( status ? "FAIL" : "OK");
+      if ( status ) 
+        {
+          va_list ap;
+          va_start (ap, fmt);
+          printf (" (");
+          vprintf (fmt, ap);
+          va_end (ap);
+          printf ("  expected %.18g observed %.18g)", y, x);
+        }
+      printf ("\n");
+    }
+  update_count_tests (status);
+  return (status ? FALSE : TRUE);
+}
+
+/** 
+ * Checks if |x - y|  < abserr
+ * 
+ * @param x computed result
+ * @param y exepcted value
+ * @param abserr maximum absolute error
+ * @param str the fonctionnality tested
+ * 
+ * @return FALSE or TRUE
+ */
+int pnl_eq_abs (double x, double y, double abserr, const char *str, const char *fmt, ...)
+{
+  int status = 0;
+  if ( (pnl_isnan (x) && !pnl_isnan(y)) ||
+       (pnl_isinf (x) && !pnl_isinf(y)) )
+    {
+      status = 1;
+    }
+  else 
+    {
+      status = (fabs (x -y) > abserr);
+    }
+  if ( status || verbose )
+    {
+      printf ("%s : ", str);
+      printf ( status ? "FAIL" : "OK");
+      if ( status ) 
+        {
+          va_list ap;
+          va_start (ap, fmt);
+          printf (" (");
+          vprintf (fmt, ap);
+          va_end (ap);
+          printf ("  expected %.18g observed %.18g)", y, x);
+        }
+      printf ("\n");
+    }
+  update_count_tests (status);
+  return (status ? FALSE : TRUE);
+}
 
 void run_all_test (tst_list *l)
 {
--- examples/tests_utils.h
+++ examples/tests_utils.h
@@ -14,5 +14,13 @@ typedef struct tst_list_t tst_list;
 extern void run_all_test (tst_list *l);
 extern void menu_test (tst_list *l);
 
+/* New unit test framework */
+extern int verbose;
+extern int pnl_eq_rel (double x, double y, double relerr, const char *label, const char *fmt, ...);
+extern int pnl_eq_abs (double x, double y, double abserr, const char *label, const char *fmt, ...);
+extern void pnl_init_tests ();
+extern int pnl_finalize_tests ();
+
+
 #endif /* TESTS_H */
 
