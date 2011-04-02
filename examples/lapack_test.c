cb8a4cc965a7c1c067880d43f80e00750f8a76e3
Jerome Lelong <jerome.lelong@gmail.com>
2011-04-02 15:32:31 +0000

    new unit test for Lapack wrappers
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pnl/trunk@686 94367915-3dfb-0310-9a54-a1fefb3e5df9

--- examples/lapack_test.c
+++ examples/lapack_test.c
@@ -66,62 +66,68 @@ static PnlMat* pnl_mat_create_diagonalizable (int n)
 
 static void pnl_mat_eigen_test ()
 {
-  PnlMat *A, *B, *P;
+  PnlMat *A, *B, *P, *Pinv, *V;
   PnlVect *v;
   int n = 4;
   int gen = PNL_RNG_MERSENNE_RANDOM_SEED;
-  printf("test de la fonction eigen : \n");
   pnl_rand_init (gen, n, n);
   A = pnl_mat_create (n, n);
   B = pnl_mat_create (n, n);
   P = pnl_mat_create (n, n);
+  Pinv = pnl_mat_create (n, n);
   pnl_mat_rand_uni2 (A, n, n, 0., 1., gen);
   pnl_mat_dgemm ('N', 'T', 1., A, A, 0., B);
   v = pnl_vect_create (0);
-  printf ("S = "); pnl_mat_print_nsp (B);
+
   pnl_mat_eigen (v, P, B, TRUE);
-  printf ("P = "); pnl_mat_print_nsp (P);
-  printf ("v = "); pnl_vect_print_nsp (v);
+  V = pnl_mat_create_diag (v);
+  pnl_mat_inverse(Pinv, P);
+  pnl_mat_mult_mat_inplace (A, P, V);  /* P V */
+  pnl_mat_mult_mat_inplace (V, A, Pinv); /* P V P^-1 */
+  pnl_test_mat_eq_abs(V, B, 1E-8, "eigen", "");
+  
+
+  pnl_mat_free (&V);
   pnl_mat_free(&A);
-  pnl_mat_free (&B);
 
   A = pnl_mat_create_diagonalizable (n);
-  printf ("A = "); pnl_mat_print_nsp (A);
   pnl_mat_eigen (v, P, A, TRUE);
-  printf ("P = "); pnl_mat_print_nsp (P);
-  printf ("v = "); pnl_vect_print_nsp (v);
+  V = pnl_mat_create_diag (v);
+  pnl_mat_inverse(Pinv, P);
+  pnl_mat_mult_mat_inplace (B, P, V);  /* P V */
+  pnl_mat_mult_mat_inplace (V, B, Pinv); /* P V P^-1 */
+  pnl_test_mat_eq_abs(V, A, 1E-8, "eigen", "");
+
   pnl_vect_free (&v);
+  pnl_mat_free (&V);
   pnl_mat_free (&A);
+  pnl_mat_free (&B);
   pnl_mat_free (&P);
+  pnl_mat_free (&Pinv);
 }
 
 static void pnl_mat_log_test ()
 {
-  PnlMat *A, *B;
+  PnlMat *A, *B, *expB;
   int n = 5;
-  printf("test de la fonction log : \n");
   A = pnl_mat_create_diagonalizable (n);
   B = pnl_mat_create (n,n);
-  printf ("A =");
-  pnl_mat_print_nsp (A);
+  expB = pnl_mat_create (n,n);
   pnl_mat_log (B, A);
-  printf ("A =");
-  pnl_mat_print_nsp (A);
-  printf("logA =");
-  pnl_mat_print_nsp (B);
-  printf("\n");
+  pnl_mat_exp (expB, B);
+  pnl_test_mat_eq_abs( expB, A, 1E-8, "logm / expm", "");
   pnl_mat_free (&B);
+  pnl_mat_free (&expB);
   pnl_mat_free (&A);
 }
 
 static void pnl_mat_ls_test ()
 {
-  PnlMat *A, *B;
-  PnlVect *b;
+  PnlMat *A, *B, *Bcopy, *AB;
+  PnlVect *b, *Ab, *bcopy;
   int m, n, gen, nrhs;
   m = 5; n = 6; nrhs = 3;
   gen = PNL_RNG_MERSENNE_RANDOM_SEED;
-  printf("Résolution de A x = b avec A non inversible: \n");
   A = pnl_mat_create (m, n);
   B = pnl_mat_create (m, nrhs);
   b = pnl_vect_create (m);
@@ -130,60 +136,65 @@ static void pnl_mat_ls_test ()
   pnl_mat_rand_uni2 (B, m, nrhs, 0, 1, gen);
   pnl_vect_rand_uni (b, m, 0, 1, gen);
 
-  printf ("A = "); pnl_mat_print_nsp(A);
-  printf ("B = "); pnl_mat_print_nsp(B);
+  Bcopy = pnl_mat_copy (B);
   if (pnl_mat_ls_mat (A, B) != OK) printf("error in pnl_mat_ls\n");
-  printf ("X = "); pnl_mat_print_nsp(B);
+  AB = pnl_mat_mult_mat (A, B);
+  pnl_test_mat_eq_abs( AB, Bcopy, 1E-8, "mat_ls_mat", "");
 
-
-  printf ("b = "); pnl_vect_print_nsp(b);
+  bcopy = pnl_vect_copy (b);
   if (pnl_mat_ls (A, b) != OK) printf("error in pnl_mat_ls\n");
-  printf ("x = "); pnl_vect_print_nsp(b);
+  Ab = pnl_mat_mult_vect (A, b);
+  pnl_test_vect_eq_abs( Ab, bcopy, 1E-8, "mat_ls", "");
 
   pnl_mat_free (&A);
+  pnl_mat_free (&AB);
   pnl_mat_free (&B);
+  pnl_mat_free (&Bcopy);
   pnl_vect_free (&b);
+  pnl_vect_free (&bcopy);
 }
 
 static void pnl_mat_qr_test ()
 {
-  PnlMat *A, *Q, *R;
+  PnlMat *A, *Ap, *Q, *R, *QR;
   PnlPermutation *p;
   int m, gen;
   m = 5;
   gen = PNL_RNG_MERSENNE_RANDOM_SEED;
   p = pnl_permutation_create (m);
   A = pnl_mat_create (m, m);
+  Ap = pnl_mat_create (m, m);
   Q = pnl_mat_create (m, m);
   R = pnl_mat_create (m, m);
+  QR = pnl_mat_create (m, m);
   pnl_rand_init (gen, m, m);
   pnl_mat_rand_uni2 (A, m, m, 0, 1, gen);
 
-  printf("QR Decomposition\n");
-  printf ("A = "); pnl_mat_print_nsp (A);
   pnl_mat_qr (Q, R, NULL, A);
-  printf ("Q = "); pnl_mat_print_nsp (Q);
-  printf ("R = "); pnl_mat_print_nsp (R);
+  pnl_mat_mult_mat_inplace (QR, Q, R);
+  pnl_test_mat_eq_abs(QR, A, 1E-8, "QR", "");
 
-  printf("QR Decomposition with pivoting\n");
   pnl_mat_qr (Q, R, p, A);
-  printf ("Q = "); pnl_mat_print_nsp (Q);
-  printf ("R = "); pnl_mat_print_nsp (R);
-  printf ("P = "); pnl_permutation_print (p);
+  pnl_mat_col_permute (Ap, A, p);
+  pnl_mat_mult_mat_inplace (QR, Q, R);
+  pnl_test_mat_eq_abs(QR, Ap, 1E-8, "QR with pivoting", "");
 
   pnl_mat_free (&A);
+  pnl_mat_free (&Ap);
   pnl_mat_free (&Q);
   pnl_mat_free (&R);
+  pnl_mat_free (&QR);
   pnl_permutation_free (&p);
 }
 
 
-int main ()
+int main (int argc, char *argv[])
 {
+  pnl_test_init(argc, argv);
   pnl_mat_eigen_test ();
   pnl_mat_log_test ();
   pnl_mat_ls_test ();
   pnl_mat_qr_test ();
-  return OK;
+  exit (pnl_test_finalize("Lapack wrappers"));
 }
 
