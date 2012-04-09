7e7c5b26b309ae364fc98fdf1487f9b6e55e0b2f
Jerome Lelong <jerome.lelong@gmail.com>
2012-04-09 08:45:23 +0000

    Update version on doxygen
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pnl/trunk@847 94367915-3dfb-0310-9a54-a1fefb3e5df9

--- src/mainpage.h
+++ src/mainpage.h
@@ -1,8 +1,9 @@
+/* vim: set fenc=utf-8: */
 
 /**
    @mainpage Pnl
-   @version 1.3
-   @author Jérôme Lelong (2007 - 2011)
+   @version 1.5
+   @author Jérôme Lelong (2007 - 2012)
    @author David Pommier (2007 - Apr. 2009)
  
    @section PnlIntro Introduction
@@ -11,7 +12,6 @@
    Lesser General Public License.
 
    A wide range of routines are availabe on the following topics
-
    - Complex Numbers
    - Cumulative Distribution Functions
    - Fast Fourier Transform
