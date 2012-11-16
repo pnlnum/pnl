9c6e0b02a9b4980ad4aa364032ca1d08e09d5ce5
Jerome Lelong <jerome.lelong@gmail.com>
2012-11-16 16:11:42 +0000

    Remove #define inline from config.h
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pnl/trunk@883 94367915-3dfb-0310-9a54-a1fefb3e5df9

--- config.h.cmake
+++ config.h.cmake
@@ -49,8 +49,3 @@
 /* Define to 1 if you use internal Blas */
 #cmakedefine USE_INTERNAL_BLAS
 
-/* Define to '__inline__' or '__inline' if that s what the C compiler
-   calls it, or to nothing if 'inline' is not supported under any name.  */
-#ifndef __cplusplus
-#define inline
-#endif
