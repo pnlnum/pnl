a857cd4d711b93ec4289a5f41629c80c1dd7926e
Jerome Lelong <jerome.lelong@gmail.com>
2015-12-27 12:20:11 +0000

    Remove extra spaces
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pnl/trunk@1078 94367915-3dfb-0310-9a54-a1fefb3e5df9

--- src/include/pnl/pnl_basis.h
+++ src/include/pnl/pnl_basis.h
@@ -52,43 +52,40 @@ struct _PnlBasis
                                           domain to [-1, 1]^nb_variates */
 };
 
-extern int pnl_basis_type_register (const char *name, double (*f)(double, int), 
-                             double (*Df)(double, int), double (*D2f)(double, int));
-extern PnlBasis* pnl_basis_new ();
-extern PnlBasis* pnl_basis_create ( int index, int nb_func, int space_dim);
-extern PnlBasis* pnl_basis_create_from_degree ( int index, int degree, int space_dim);
-extern PnlBasis* pnl_basis_create_from_prod_degree (int index, int degree, int nb_variates);
-extern PnlBasis* pnl_basis_create_from_hyperbolic_degree (int index, double degree, double q, int n);
-extern void pnl_basis_clone (PnlBasis *dest, const PnlBasis *src);
-extern PnlBasis* pnl_basis_copy (const PnlBasis *B);
-extern void  pnl_basis_set_from_tensor (PnlBasis *b, int index, const PnlMatInt *T);
-extern PnlBasis* pnl_basis_create_from_tensor ( int index, const PnlMatInt *T);
-extern void pnl_basis_del_elt (PnlBasis *B, const PnlVectInt *d);
-extern void pnl_basis_del_elt_i (PnlBasis *B, int i);
-extern void pnl_basis_add_elt (PnlBasis *B, const PnlVectInt *d);
-extern void pnl_basis_set_domain (PnlBasis *B, const PnlVect *xmin, const PnlVect *xmax);
-extern void pnl_basis_set_reduced (PnlBasis *B, const PnlVect *center, const PnlVect *scale);
-extern void pnl_basis_free (PnlBasis **basis);
-extern void pnl_basis_print (const PnlBasis *B);
-extern int pnl_basis_fit_ls (const PnlBasis *f, PnlVect *coef, const PnlMat *x, const PnlVect *y);
-extern double pnl_basis_ik_vect (const PnlBasis *b, const PnlVect *x, int i, int k);
-extern double pnl_basis_i_vect (const PnlBasis *b, const PnlVect *x, int i);
-extern double pnl_basis_i_D_vect (const PnlBasis *b, const PnlVect *x, int i, int j);
-extern double pnl_basis_i_D2_vect (const PnlBasis *b, const PnlVect *x, int i, int j1, int j2);
-extern double pnl_basis_eval_vect (const PnlBasis *basis, const PnlVect *coef, const PnlVect *x);
-extern double pnl_basis_eval_D_vect (const PnlBasis *basis, const PnlVect *coef, const PnlVect *x, int i);
-extern double pnl_basis_eval_D2_vect (const PnlBasis *basis, const PnlVect *coef, const PnlVect *x, int i, int j);
-extern void pnl_basis_eval_derivs_vect (const PnlBasis *b, const PnlVect *coef, const PnlVect *x,
-                                        double *val, PnlVect *grad, PnlMat *hes);
-extern double pnl_basis_ik (const PnlBasis *b, const double *x, int i, int k);
-extern double pnl_basis_i ( const PnlBasis *b, const double *x, int i );
-extern double pnl_basis_i_D ( const PnlBasis *b, const double *x, int i, int j );
-extern double pnl_basis_i_D2 (const PnlBasis *b, const double *x, int i, int j1, int j2);
-extern double pnl_basis_eval (const PnlBasis *basis, const PnlVect *coef, const double *x);
-extern double pnl_basis_eval_D (const PnlBasis *basis, const PnlVect *coef, const double *x, int i);
-extern double pnl_basis_eval_D2 (const PnlBasis *basis, const PnlVect *coef, const double *x, int i, int j);
-extern void pnl_basis_eval_derivs (const PnlBasis *basis, const PnlVect *coef, const double *x,
-                                   double *val, PnlVect *grad, PnlMat *hes);
+extern int pnl_basis_type_register(const char *name, double (*f)(double, int), double (*Df)(double, int), double (*D2f)(double, int));
+extern PnlBasis* pnl_basis_new();
+extern PnlBasis* pnl_basis_create(int index, int nb_func, int space_dim);
+extern PnlBasis* pnl_basis_create_from_degree(int index, int degree, int space_dim);
+extern PnlBasis* pnl_basis_create_from_prod_degree(int index, int degree, int nb_variates);
+extern PnlBasis* pnl_basis_create_from_hyperbolic_degree(int index, double degree, double q, int n);
+extern void pnl_basis_clone(PnlBasis *dest, const PnlBasis *src);
+extern PnlBasis* pnl_basis_copy(const PnlBasis *B);
+extern void  pnl_basis_set_from_tensor(PnlBasis *b, int index, const PnlMatInt *T);
+extern PnlBasis* pnl_basis_create_from_tensor( int index, const PnlMatInt *T);
+extern void pnl_basis_del_elt(PnlBasis *B, const PnlVectInt *d);
+extern void pnl_basis_del_elt_i(PnlBasis *B, int i);
+extern void pnl_basis_add_elt(PnlBasis *B, const PnlVectInt *d);
+extern void pnl_basis_set_domain(PnlBasis *B, const PnlVect *xmin, const PnlVect *xmax);
+extern void pnl_basis_set_reduced(PnlBasis *B, const PnlVect *center, const PnlVect *scale);
+extern void pnl_basis_free(PnlBasis **basis);
+extern void pnl_basis_print(const PnlBasis *B);
+extern int pnl_basis_fit_ls(const PnlBasis *f, PnlVect *coef, const PnlMat *x, const PnlVect *y);
+extern double pnl_basis_ik_vect(const PnlBasis *b, const PnlVect *x, int i, int k);
+extern double pnl_basis_i_vect(const PnlBasis *b, const PnlVect *x, int i);
+extern double pnl_basis_i_D_vect(const PnlBasis *b, const PnlVect *x, int i, int j);
+extern double pnl_basis_i_D2_vect(const PnlBasis *b, const PnlVect *x, int i, int j1, int j2);
+extern double pnl_basis_eval_vect(const PnlBasis *basis, const PnlVect *coef, const PnlVect *x);
+extern double pnl_basis_eval_D_vect(const PnlBasis *basis, const PnlVect *coef, const PnlVect *x, int i);
+extern double pnl_basis_eval_D2_vect(const PnlBasis *basis, const PnlVect *coef, const PnlVect *x, int i, int j);
+extern void pnl_basis_eval_derivs_vect(const PnlBasis *b, const PnlVect *coef, const PnlVect *x, double *val, PnlVect *grad, PnlMat *hes);
+extern double pnl_basis_ik(const PnlBasis *b, const double *x, int i, int k);
+extern double pnl_basis_i( const PnlBasis *b, const double *x, int i );
+extern double pnl_basis_i_D( const PnlBasis *b, const double *x, int i, int j );
+extern double pnl_basis_i_D2(const PnlBasis *b, const double *x, int i, int j1, int j2);
+extern double pnl_basis_eval(const PnlBasis *basis, const PnlVect *coef, const double *x);
+extern double pnl_basis_eval_D(const PnlBasis *basis, const PnlVect *coef, const double *x, int i);
+extern double pnl_basis_eval_D2(const PnlBasis *basis, const PnlVect *coef, const double *x, int i, int j);
+extern void pnl_basis_eval_derivs(const PnlBasis *basis, const PnlVect *coef, const double *x, double *val, PnlVect *grad, PnlMat *hes);
 
 
 /*@}*/
