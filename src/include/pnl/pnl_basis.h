3bdce3570509be3d0720ff8c9bc1bca49bc3661a
Jerome Lelong <jerome.lelong@gmail.com>
2016-03-11 11:12:44 +0000

    Inline pnl_basis_i
    For small degree polynomial, the gain is around 20%.
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pnl/trunk@1097 94367915-3dfb-0310-9a54-a1fefb3e5df9

--- src/include/pnl/pnl_basis.h
+++ src/include/pnl/pnl_basis.h
@@ -113,14 +113,60 @@ extern double pnl_basis_eval_D_vect(const PnlBasis *basis, const PnlVect *coef, 
 extern double pnl_basis_eval_D2_vect(const PnlBasis *basis, const PnlVect *coef, const PnlVect *x, int i, int j);
 extern void pnl_basis_eval_derivs_vect(const PnlBasis *b, const PnlVect *coef, const PnlVect *x, double *val, PnlVect *grad, PnlMat *hes);
 extern double pnl_basis_ik(const PnlBasis *b, const double *x, int i, int k);
-extern double pnl_basis_i( const PnlBasis *b, const double *x, int i );
-extern double pnl_basis_i_D( const PnlBasis *b, const double *x, int i, int j );
+extern double pnl_basis_i_D(const PnlBasis *b, const double *x, int i, int j);
 extern double pnl_basis_i_D2(const PnlBasis *b, const double *x, int i, int j1, int j2);
 extern double pnl_basis_eval(const PnlBasis *basis, const PnlVect *coef, const double *x);
 extern double pnl_basis_eval_D(const PnlBasis *basis, const PnlVect *coef, const double *x, int i);
 extern double pnl_basis_eval_D2(const PnlBasis *basis, const PnlVect *coef, const double *x, int i, int j);
 extern void pnl_basis_eval_derivs(const PnlBasis *basis, const PnlVect *coef, const double *x, double *val, PnlVect *grad, PnlMat *hes);
 extern void pnl_basis_add_function(PnlBasis *b, PnlRnFuncR *f);
+
+#include "pnl/pnl_config.h"
+#ifdef PNL_HAVE_INLINE
+/**
+ * Evaluate the i-th element of the basis b at the point x
+ *
+ * @deprecated Use the function pnl_basis_i_vect(const PnlBasis *b, const PnlVect *x, int i)
+ *
+ * @param b a PnlBasis
+ * @param x a C array containing the coordinates of the point at which to
+ * evaluate the basis
+ * @param i an integer describing the index of the element of the basis to
+ * considier
+ *
+ * @return f_i(x) where f is the i-th basis function
+ */
+PNL_INLINE_FUNC double pnl_basis_i(const PnlBasis *b, const double *x, int i)
+{
+  int k;
+  double aux = 1.;
+  if (i > b->len_T - 1)
+    {
+      PnlVect view = pnl_vect_wrap_array(x, b->nb_variates);
+      return PNL_EVAL_RNFUNCR(&(b->func_list[i-b->len_T]), &view);
+    }
+  if (b->isreduced == 1)
+    {
+      for (k = b->SpT->I[i] ; k < b->SpT->I[i + 1] ; k++)
+        {
+          const int j = b->SpT->J[k];
+          const int Tij = b->SpT->array[k];
+          aux *= (b->f)((x[j] - b->center[j]) * b->scale[j], Tij);
+        }
+    }
+  else
+    {
+      for (k = b->SpT->I[i] ; k < b->SpT->I[i + 1] ; k++)
+        {
+          const int j = b->SpT->J[k];
+          const int Tij = b->SpT->array[k];
+          aux *= (b->f)(x[j], Tij);
+        }
+    }
+  return aux;
+}
+#endif
+PNL_INLINE_DECL double pnl_basis_i(const PnlBasis *b, const double *x, int i);
 
 /*@}*/
 
