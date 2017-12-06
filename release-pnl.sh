ab13fd6a615a57a68062fac066c6e5133e15ac20
Jerome Lelong <jerome.lelong@gmail.com>
2017-12-06 09:59:39 +0100

    Improve release script

--- release-pnl.sh
+++ release-pnl.sh
@@ -5,6 +5,7 @@
 
 CWD="$(pwd)/$0"
 PNLGIT=$(dirname $"CWD")
+MINGW_PREFIX="/usr/local/mingw64"
 
 DATE=$(date +%Y%m%d)
 # Local temporary directory
@@ -59,6 +60,7 @@ process_options() {
 set_dirs() {
     PNL_DIR="$LOCAL_TMPDIR/pnl-$VERSION"
     PNL_WINDIR="$LOCAL_TMPDIR/pnl-win64-$VERSION"
+    PNL_WINDIR="$LOCAL_TMPDIR/pnl-win64-$VERSION"
 }
 
 # Perform git archive
@@ -68,39 +70,34 @@ git_archive() {
     mkdir -p "$PNL_DIR"
     cd "$PNLGIT"
     git archive --format=tar master | tar -x -C "$PNL_DIR"
+    mv $PNL_DIR/docs/manual-html $PNL_DIR
+    mv $PNL_DIR/docs/pnl-manual.pdf $PNL_DIR
     rm -rf "$PNL_DIR/docs"
     cd "$cwd"
-}
-
-# Compile the manual
-# Arg 1: top level directory
-compile_manual() {
-    cd $1
-    make -C man
-    make -C man clean
 }
 
 create_win_version() {
     cwd=$(pwd)
     mkdir -p "$PNL_WINDIR"
-    cd "$PNL_WINDIR/.."
-    mkdir -p "$PNL_WINDIR/../build-win"
-    cd "$PNL_WINDIR/../build-win"
-    source /usr/local/src/mingw64-build/withmingw
+    mkdir -p "$LOCAL_TMPDIR/build-win"
+    cd "$LOCAL_TMPDIR/build-win"
+    PATH="$MINGW_PREFIX/bin:$MINGW_PREFIX/x86_64-w64-mingw32/bin:$PATH"
     cmake -DCROSS_COMPILE=ON -DCMAKE_BUILD_TYPE=Release -DPNL_INSTALL_PREFIX="$PNL_WINDIR" "$PNL_DIR"
-    make
-    make install
+    make || { echo "make failed" && exit 1; }
+    make install || { echo "make install failed" && exit 1; }
     cd "$PNL_WINDIR"
     mv lib/libpnl.dll.a lib/libpnl.lib
     # Copy the manual
-    cp -r "$PNL_DIR/man" "$PNL_WINDIR"
+    cp -r "$PNL_DIR/manual-html" "$PNL_WINDIR"
+    cp -r "$PNL_DIR/pnl-manual.pdf" "$PNL_WINDIR"
     # Copy all required dll's
     LIBS="libblas.dll libgcc_s_seh-1.dll libgfortran-3.dll liblapack.dll libquadmath-0.dll"
     for lib in $LIBS; do
-        cp /usr/local/mingw64/x86_64-w64-mingw32/bin/$lib lib
+        cp $MINGW_PREFIX/x86_64-w64-mingw32/bin/$lib lib
     done
     cd "$LOCAL_TMPDIR"
-    zip -r "pnl-win64-$VERSION.zip" "pnl-win64-$VERSION"
+    [[ -f "pnl-win64-$VERSION.zip" ]] && rm "pnl-win64-$VERSION.zip"
+    zip -q -r "pnl-win64-$VERSION.zip" "pnl-win64-$VERSION"
     # Restore working directory
     cd "$cwd"
 }
