35115450607af86dd395d8ab0c1bd326809c5159
Jerome Lelong <jerome.lelong@gmail.com>
2019-04-12 09:58:33 +0200

    Update extract-h

--- extract-h.sh
+++ extract-h.sh
@@ -7,9 +7,7 @@
 #   item \describefun{int}{pnl_vect_complex_isequal_rel}{const \PnlVectComplex \ptr x, const \PnlVectComplex \ptr y, double relerr} 
 
 sed -E 's/ *extern *//g' | \
-sed -E 's/([a-zA-Z0-9_ \*]+) +([a-zA-Z0-9_]+) *\((.*)\);/\\item \\describefun{\1}{\2}{\3}/g' | \
+sed -E 's/([a-zA-Z0-9_ \*]+) +([a-zA-Z0-9_]+) *\((.*)\);?/\\item \\describefun{\1}{\2}{\3}/g' | \
 sed -E 's/\*/\\ptr /g' | \
 sed -E 's/(Pnl[a-zA-Z]+)/\\\1/g' | \
 sed -E 's/dcomplex/\\dcomplex/g'
-    
-
